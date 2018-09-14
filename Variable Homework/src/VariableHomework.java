7d6965a0b41ecc2ca41abdebe10b7866de6bac0a
Radle <Radle@192.168.0.13>
2018-09-13 22:19:51 -0400

    Created a project to display name, area of triangle, rectangle and circle , and circumference of circle

---
+++ Variable Homework/src/VariableHomework.java
@@ -0,0 +1,30 @@
+
+public class VariableHomework {
+
+	public static void main(String[] args)
+	{
+		String name;
+		name = "Ian";
+		System.out.print("My name is ");
+		System.out.println(name);
+				
+		int height = 6;
+		int base = 8;
+		System.out.print("The area is ");
+		System.out.println(height * base/2);
+		
+		height = 12;
+		base  = 6;
+		System.out.print(height * base);
+		
+		int radius = 7;
+		double pi = 3.14;
+		System.out.print("The circumference is ");
+		System.out.println(2 * radius * pi);
+	    System.out.print("The area is ");
+	    System.out.print(Math.pow(radius, 2) * pi);
+				
+
+	}
+
+}
